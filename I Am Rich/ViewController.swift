3811f4e6d708d0c83069d977d17be93e83038747
Kristy Li <46299010+ahtae@users.noreply.github.com>
2019-07-22 20:55:54 -0400

    Add files via upload

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kristy Li on 6/16/19.
+//  Copyright © 2019 Kristy Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
